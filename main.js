73600c0f184f4813efa0cccfe4f99e6d5e98bc9c
Renan Freitas <89546855+Refusado@users.noreply.github.com>
2022-04-16 03:52:36 -0300

    (front-end) layout is done

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-var height = 6; // número de tentativas
-var width = 5; // quantidade de letras da palavra
+var height = 6; // número de tentativas do jogo atual  | acima de 14 tentativas fica ruim enxergar
+var width = 5; // quantidade de letras da palavra      | acima de 14 letras fica ruim enxergar
 
 var root = document.documentElement;
 root.style.setProperty('--board-rows', height);
 root.style.setProperty('--board-columns', width);
 
+
 var row = 0; // tentativa atual (linha que o jogador está)
 var col = 0; // letra atual (coluna que o jogador está)
+
 
 var gameOver = false;
 var word = "PERNA";
@@ -31,7 +33,7 @@ function intialize() {
   // pega a telca pressionada
   document.addEventListener("keyup", (e) => {
     if (gameOver) return;
-
+    
     if ("KeyA" <= e.code && e.code <= "KeyZ"){
       if (col < width) {
         let currTile = document.getElementById(row.toString() + '-' + col.toString());
@@ -54,4 +56,3 @@ function intialize() {
 
 
 }
-
