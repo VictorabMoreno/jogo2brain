f90710281c7e923c7b154e2c62dd2eaebfdec03f
Renan Freitas <89546855+Refusado@users.noreply.github.com>
2022-04-17 07:06:30 -0300

    (front-end) teclado adicionado

--- main.js
+++ main.js
@@ -33,50 +33,97 @@ function intialize() {
     }
   }
 
-  document.addEventListener("keyup", (e) => {
-    // pega a tecla pressionada
-    if (gameOver) return;
-    
-    if ("KeyA" <= e.code && e.code <= "KeyZ" || e.code == "Semicolon"){
-      // define as teclas permitidas e insere elas na caixa quando a pessoa teclar
-      if (col < width) {
-        let currTile = document.getElementById(row.toString() + '-' + col.toString());
-        if (currTile.innerText == "" && e.code != "Semicolon") {
-          currTile.innerText = e.code[3];
-          col += 1;
-        }
-        else if (currTile.innerText == "" && e.code == "Semicolon") {
-          // especificando o comportamento do ç
-          currTile.innerText = "Ç";
-          col += 1;
-        }
-      }
-    }
-    else if (e.code == "Backspace") {
-      // remove a letra do box ao pressionar o backspace
-      if (0 < col && col <= width) {
-        col -= 1;
-      }
-      let currTile = document.getElementById(row.toString() + '-' + col.toString());
-      currTile.innerText = "";
-    }
-    else if (e.code == "Enter" && col == width) {
-      // envia a tentativa ao pressinar enter
-      update();
-      row += 1;
-      col = 0;
-    }
-    
-    if (!gameOver && row == height) {
-      // se o jogador já usou todas as tentativas
-      gameOver = true;
-      console.log("Jogo fracassado");
+  // cria o teclado
+  let keyboard = [
+    ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+    ["A", "S", "D", "F", "G", "H", "J", "K", "L", "⌫"],
+    ["Z", "X", "C", "V", "B", "N", "M", "Enter"]
+  ]
 
-      document.getElementById("answer").innerHTML = word; // insere a palavra secreta no html
-      setCss(document.getElementById("answer"), "display", "inline");
-      setCss(document.getElementById("notify-container"), "visibility", "visible");
+  for (let i = 0; i < keyboard.length; i++){
+    let currRow = keyboard[i];
+    let keyboardRow = document.createElement("div");
+    keyboardRow.classList.add("keyboard-row");
+
+    for (let j = 0; j < currRow.length; j++) {
+      let keyTile = document.createElement("div");
+
+      let key = currRow[j];
+      keyTile.innerText = key;
+      if (key == "Enter") {
+        keyTile.id = "Enter";
+      }
+      else if (key == "⌫") {
+        keyTile.id = "Backspace";
+      }
+      else if ("A" <= key && key <= "Z") {
+        keyTile.id = "Key" + key;
+      }
+      
+      keyTile.addEventListener("click", processKey);
+
+      if (key == "Enter") {
+        keyTile.classList.add("enter-key-tile");
+      }
+      else {
+        keyTile.classList.add("key-tile")
+      }
+      keyboardRow.appendChild(keyTile);
     }
+    document.getElementById("keyboard").appendChild(keyboardRow);
+  }
+
+  document.addEventListener("keyup", (e) => {
+    processInput(e);
   });
+}
+
+function processKey() {
+  e = {"code" : this.id};
+  processInput(e);
+}
+
+function processInput(e) {
+  // pega a tecla pressionada
+  if (gameOver) return;
+  
+  if ("KeyA" <= e.code && e.code <= "KeyZ" || e.code == "Semicolon"){
+    // define as teclas permitidas e insere elas na caixa quando a pessoa teclar
+    if (col < width) {
+      let currTile = document.getElementById(row.toString() + '-' + col.toString());
+      if (currTile.innerText == "" && e.code != "Semicolon") {
+        currTile.innerText = e.code[3];
+        col += 1;
+      }
+      else if (currTile.innerText == "" && e.code == "Semicolon") {
+        // especificando o comportamento do ç
+        currTile.innerText = "Ç";
+        col += 1;
+      }
+    }
+  }
+  else if (e.code == "Backspace") {
+    // remove a letra do box ao pressionar o backspace
+    if (0 < col && col <= width) {
+      col -= 1;
+    }
+    let currTile = document.getElementById(row.toString() + '-' + col.toString());
+    currTile.innerText = "";
+  }
+  else if (e.code == "Enter" && col == width) {
+    // envia a tentativa ao pressinar enter
+    update();
+  }
+  
+  if (!gameOver && row == height) {
+    // se o jogador já usou todas as tentativas
+    gameOver = true;
+    console.log("Jogo fracassado");
+
+    document.getElementById("answer").innerHTML = word; // insere a palavra secreta no html
+    setCss(document.getElementById("answer"), "display", "inline");
+    setCss(document.getElementById("notify-container"), "visibility", "visible");
+  }
 }
 
 function update() {
@@ -100,6 +147,11 @@ function update() {
 
     if (word[c] == letter) {
       currTile.classList.add("correct");
+
+      let keyTile = document.getElementById("Key" + letter);
+      keyTile.classList.remove("present");
+      keyTile.classList.add("correct");
+
       correct += 1;
       letterCount[letter] -= 1;
     }
@@ -123,11 +175,20 @@ function update() {
     if (!currTile.classList.contains("correct")) {
       if (word.includes(letter) && letterCount[letter] > 0) {
         currTile.classList.add("present");
+
+        let keyTile = document.getElementById("Key" + letter);
+        if (!keyTile.classList.contains("correct")) {
+          keyTile.classList.add("present");
+        }
         letterCount[letter] -= 1;
       }
       else {
         currTile.classList.add("absent");
+        let keyTile = document.getElementById("Key" + letter);
+        keyTile.classList.add("absent")
       }
     }
   }
+  row += 1;
+  col = 0;
 }
